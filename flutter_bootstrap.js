1e34da3850947d21f11c0c806eb5f2356c02ec6c
body1108366 <230241126+body1108366@users.noreply.github.com>
2025-12-25 19:42:04 +0200

    latest

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2875183520"
+    serviceWorkerVersion: "1384677951"
   }
 });
