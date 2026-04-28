5e97a1a0050f49651ade179e07fbfbd700b55df0
body1108366 <230241126+body1108366@users.noreply.github.com>
2026-04-28 15:51:37 +0300

    New weeklyPlan

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "5bf3572e595feec1a98e8b732b55d025
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "1c7e1625238c70e6130c2fbf3dcaf76c",
+"flutter_bootstrap.js": "9b85a85165a341ce96079249ed0e704b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
