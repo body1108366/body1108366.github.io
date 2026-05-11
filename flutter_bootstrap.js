f6847ac1b9c68f19bd5e5e6d12601f2a7ae93b38
body1108366 <230241126+body1108366@users.noreply.github.com>
2026-05-12 00:26:15 +0300

    f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1068359467"
+    serviceWorkerVersion: "2236688546"
   }
 });
