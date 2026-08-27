abc5e6860d00eeef6b7cbf22b55ff6dfa960c0b3
body1108366 <taskora083@gmail.com>
2026-08-27 16:19:37 +0300

    fixed_team_plans

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4202283967" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3792477902" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
