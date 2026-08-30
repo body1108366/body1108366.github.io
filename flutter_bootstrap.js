8c4d124bde082c0265c399cfa098987822b507b1
body1108366 <taskora083@gmail.com>
2026-08-30 15:51:22 +0300

    fixed bugs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3792477902" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2504498427" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
