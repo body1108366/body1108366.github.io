27e3c5fe8d2acadf2fb5f7f2396eddb33be06985
body1108366 <taskora083@gmail.com>
2026-09-01 15:38:55 +0300

    new

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+// web/firebase-messaging-sw.js
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js",
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js",
+);
+
+// We leave this file empty of keys. The browser just needs to see it exists!
